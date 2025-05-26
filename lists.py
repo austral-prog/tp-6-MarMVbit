83840286646212f9a9094904a0cb667981a880fd
MarMVbit <mmvillacorta@mail.austral.edu.ar>
2025-05-25 21:09:38 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,38 @@
-# Replace the "ANSWER HERE" with your answer
-
 def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(list_to_remove_elements) > 5:
+        del list_to_remove_elements[5]
+    elif len(list_to_remove_elements) > 4:
+        del list_to_remove_elements[4]
+    elif len(list_to_remove_elements) > 0:
+        del list_to_remove_elements[0]
+    return list_to_remove_elements
+    
+print(remove_elements(['Red', 'Green', 'White', 'Black', 'Pink', 'Yellow']))
 
 def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    list_to_add_elements.append('Yellow')
+    list_to_add_elements.insert(0, 'Pink')
+    return list_to_add_elements
+print(add_elements(['Red', 'Green', 'White', 'Black']))
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    return list_to_check == []
+print(is_empty([]))
+print(is_empty(['Red', 'Green', 'White', 'Black']))
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if len(list_to_compare1) < 3 or len(list_to_compare2) < 3:
+        return False
+    else:
+        if list_to_compare1[2] == list_to_compare2[2]:
+            return True
+        else:
+            return False
+print(check_lists(['Black', 'Pink', 'Red', 'Green', 'White'], ['Red', 'Green', 'Yellow', 'White', 'Black', 'Pink']))
 
 def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+    list_of_lists_to_modify[0] = list_of_lists_to_modify[0][0:2]
+    list_of_lists_to_modify[1] = list_of_lists_to_modify[1][1:4]
+    list_of_lists_to_modify[2] = list_of_lists_to_modify[2][-2:]
+    return list_of_lists_to_modify
+print(list_of_lists([[1, 2, 3], [4, 5, 6, 7, 8], [9, 10, 11, 12]]))
